e399b1b6d12b53dec280d6645c9d0dd2775e823a
Vina del Mar <vina@duoc.acad>
2023-08-14 13:43:48 -0400

    primer commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+  standalone: true,
+  imports: [IonicModule],
+})
+
+export class HomePage {
+  title = 'Tour of Heroes';
+  myHero = 'Windstorm';
+  heroes =  ['Windstorm', 'Bombasto', 'Magneta', 'Tornado'];
+  constructor() {}
+}
